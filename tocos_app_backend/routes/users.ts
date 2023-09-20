7f5d0c066faf0cb9575925cf413c6a396316297c
Ikechi Anya <ikechi@agures.com>
2023-09-20 22:36:27 +0100

    Vercel check dder

--- tocos_app_backend/routes/users.ts
+++ tocos_app_backend/routes/users.ts
@@ -6,21 +6,31 @@ const router = users_express.Router()
 
 router.get('/users', async (req, res) => {
   let client;
-  let status: NetworkStatus = 200;
-  client = await db.connect();
   try {
+    client = await db.connect();
     // await client.sql`DROP TABLE IF EXISTS Users`
     await client.sql`CREATE TABLE IF NOT EXISTS Users ( id serial PRIMARY KEY, tocos integer )`
     await client.sql`INSERT INTO Users (tocos) VALUES (2500)`
   } catch(error) {
-    status = 500;
-    res.status(status).send((networkResponse('error', error)));
+    return res.status(500).send((networkResponse('error', error)));
   }
 
-  if (status === 200) {
-    const users = await client.sql`Select * from Users`
-    res.status(status).send((networkResponse('success', users.rows.length)));
+  const users = await client.sql`Select * from Users`
+  return res.status(200).send((networkResponse('success', users.rows.length)));
+})
+
+router.get('/users/{id}', async (req, res) => {
+  let client;
+  try {
+    client = await db.connect();
+    await client.sql`CREATE TABLE IF NOT EXISTS Users ( id serial PRIMARY KEY, tocos integer )`
+    await client.sql`INSERT INTO Users (tocos) VALUES (2500)`
+  } catch(error) {
+    return res.status(500).send((networkResponse('error', error)));
   }
+
+  const users = await client.sql`Select * from Users`
+  return res.status(200).send((networkResponse('success', users.rows.length)));
 })
 
 module.exports = router
