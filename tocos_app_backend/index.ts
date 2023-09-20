cb238e9d9889f73ad0d6d512001e4f753844f6a4
Ikechi Anya <ikechi@agures.com>
2023-09-20 21:22:01 +0100

    Postgres check 6

--- tocos_app_backend/index.ts
+++ tocos_app_backend/index.ts
@@ -1,17 +1,25 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const client = require('./connection')
+// const client = require('./connection')
 const app = express();
 ['users'].map((endPoint)=> app.use(`/`, require(`./routes/${endPoint}`)))
 require('dotenv').config()
+import { db } from "@vercel/postgres";
 
 app.use(cors())
 
 const port = process.env.PORT || 8000
 
-client.connect();
-app.get('/', (req, res) => {
+// client.connect();
+app.get('/', async (req, res) => {
+  const client = await db.connect();
+  // try {
+  //   await client.sql`CREATE TABLE IF NOT EXISTS Users ( id serial PRIMARY KEY, tocos integer )`
+  //   await client.sql`INSERT INTO Users VALUES (1, 250)`
+  // } catch(error) {
+  //   res.send(`ERROR HERE ${error}`)
+  // }
   // client.query('Select * from users', (err, result) => {
   //   if (!err) {
   //     res.send(result.rows)
@@ -19,7 +27,8 @@ app.get('/', (req, res) => {
   //     console.log('ERR', err)
   //   }
   // })
-  res.send("Hello world")
+  const users = await client.sql`Select * from Users`
+  res.send(users.rows);
 })
 
 app.listen(port, () => {
