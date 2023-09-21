1f08738a556967e860b5c062b49bdcd21dfa31c6
Ikechi Anya <ikechi@agures.com>
2023-09-21 01:00:39 +0100

    Transactions complete 2

--- tocos_app_backend/routes/transactions.ts
+++ tocos_app_backend/routes/transactions.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/transactions', async (req, res) => {
   const requestBody = req.body;
   const senderId = requestBody.senderId;
-  const recieverId = requestBody.recieverId;
+  const receiverId = requestBody.receiverId;
   const tocos = Number(requestBody.tocos);
   try {
     const client = await db.connect();
@@ -16,8 +16,8 @@ router.post('/transactions', async (req, res) => {
     if (Number(senderId) > allUsersLength) {
       return res.status(400).json((networkResponse('error', 'Sender Id does not exist')));
     } 
-    if (Number(recieverId) > allUsersLength) {
-      return res.status(400).json((networkResponse('error', 'Reciever Id does not exist')));
+    if (Number(receiverId) > allUsersLength) {
+      return res.status(400).json((networkResponse('error', 'Receiver Id does not exist')));
     }
 
     const senderData = await client.sql`Select tocos from Users WHERE id=${senderId}`
@@ -28,10 +28,10 @@ router.post('/transactions', async (req, res) => {
     }
     await client.sql`UPDATE Users SET tocos = ${newSenderTocos} WHERE id=${senderId}`
   
-    const recieverData = await client.sql`Select tocos from Users WHERE id=${recieverId}`
-    const recieverTocos = Number(recieverData.rows[0].tocos);
-    const newRecieverTocos = recieverTocos + tocos;
-    await client.sql`UPDATE Users SET tocos = ${newRecieverTocos} WHERE id=${recieverId}`
+    const receiverData = await client.sql`Select tocos from Users WHERE id=${receiverId}`
+    const receiverTocos = Number(receiverData.rows[0].tocos);
+    const newreceiverTocos = receiverTocos + tocos;
+    await client.sql`UPDATE Users SET tocos = ${newreceiverTocos} WHERE id=${receiverId}`
 
     res.status(200).json((networkResponse('success', true)));
   } catch(error) {
