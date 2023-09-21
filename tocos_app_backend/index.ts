211839d5f893d7f648159b6018b9c65f0d2c6c1b
Ikechi Anya <ikechi@agures.com>
2023-09-21 17:13:00 +0100

    backend fix sdsdsf 2

--- tocos_app_backend/index.ts
+++ tocos_app_backend/index.ts
@@ -2,16 +2,16 @@
 const main_express = require('express')
 const cors = require('cors')
 const app = main_express();
-app.use(main_express.urlencoded());
-app.use(main_express.json());
-['users', 'transactions'].map((endPoint)=> app.use(`/`, require(`./routes/${endPoint}`)))
-require('dotenv').config();
 const corsOrigin ={
     origin:'http://localhost:3000', //or whatever port your frontend is using
     credentials:true,            
     optionSuccessStatus:200
 }
 app.use(cors(corsOrigin))
+app.use(main_express.urlencoded());
+app.use(main_express.json());
+['users', 'transactions'].map((endPoint)=> app.use(`/`, require(`./routes/${endPoint}`)))
+require('dotenv').config();
 
 const port = process.env.PORT || 8000
 
